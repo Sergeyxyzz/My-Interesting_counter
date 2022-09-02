0fe480110a81ad4a5f7de6f770d9d4dfd263d7ac
Sergey <sergeyxyzz@yandex.ru>
2022-09-02 17:55:48 +0300

    добавил чтобы не было больше или меньше мин и макс значения

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,33 +1,43 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 const Counter = ({ min, max }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(min);
 
   const plusCounter = () => {
     if (counter < max) {
-      setCounter(counter + 1)
+      setCounter(counter + 1);
+    }
+  };
+
+  const normCounter = () => {
+    if (counter > max) {
+      setCounter(max)
+    } else if (counter < min) {
+      setCounter(min)
     }
   }
 
   const minusCounter = () => {
     if (counter > min) {
-      setCounter(counter - 1)
+      setCounter(counter - 1);
     }
-  }
+  };
 
   const resetCounter = () => {
-    setCounter(0)
-  }
- 
+    setCounter(0);
+  };
+
   const changeInp = (e) => {
     setCounter(e);
     if (isNaN(counter)) {
-      setCounter('')
+      setCounter('');
     }
   };
+
+  normCounter()
   return (
     <div>
-      {counter}
       <button onClick={() => plusCounter()}>+</button>
       <input value={Number(counter)} onChange={(e) => changeInp(e.target.value)} />
       <button onClick={() => minusCounter()}>-</button>
